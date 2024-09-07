54a55d36ee5e6bbafffb6210610ad10ada509985
Saif Ur Rehman <saifsunnyd828@gmail.com>
2024-09-07 22:46:57 +0500

    Hackathon_Milestone_Resume
    In this Heckathon, I have created a Resume by using HTML,CSS & Typescript, it is Responsive.

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+function toggleSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.style.display = section.style.display === 'none' ? 'block' : 'none';
+    }
+}
